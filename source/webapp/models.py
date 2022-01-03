5012aac7d70b5ba95035e17b67c840ffdebc4a89
Chinara <nishanova73@mail.ru>
2022-01-03 09:33:20 +0600

    Created Task model

---
+++ source/webapp/models.py
@@ -0,0 +1,19 @@
+from django.db import models
+
+# Create your models here.
+STATUS_CHOICES = [('new', 'new'), ('in_progress', 'in progress'),  ('done', 'done')]
+
+class Task(models.Model):
+    description = models.CharField(max_length=200, null=False, blank=False, verbose_name='description')
+    status = models.CharField(max_length=50, null=False, blank=False, default='new', verbose_name='status')
+    # detailed_description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='detailed_description')
+    create_until = models.DateField(auto_now_add=True, verbose_name='Create_until')
+    status = models.CharField(max_length=50, null=False, blank=False, default='new', choices=STATUS_CHOICES)
+
+    def __str__(self):
+        return f"{self.pk}. {self.description}: {self.status}"
+
+    class Meta:
+        db_table = 'Tasks'
+        verbose_name = 'task'
+        verbose_name_plural = 'tasks'
