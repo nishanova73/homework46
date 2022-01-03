709ac49b78dbbd8903fefbabc0f984a1c986e0bf
Chinara <nishanova73@mail.ru>
2022-01-03 13:53:25 +0600

    Created names for urls and will use it in future

--- source/webapp/urls.py
+++ source/webapp/urls.py
@@ -2,8 +2,8 @@ from django.urls import path
 from webapp.views import main_view, tasks_view, create_task_view, task_view
 
 urlpatterns = [
-    path('', main_view),
-    path('task/', tasks_view),
-    path('task/create_task/', create_task_view),
-    path('task_view/<int:pk>/', task_view),
+    path('', main_view, name="main"),
+    path('task/', tasks_view, name="home"),
+    path('task/create_task/', create_task_view, name="task_add"),
+    path('task_view/<int:pk>/', task_view, name="task_view"),
 ]
