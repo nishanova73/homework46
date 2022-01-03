ae805de966bd28403089c1c821a005ffb1df6647
Chinara <nishanova73@mail.ru>
2022-01-03 10:31:34 +0600

    Modified status column

--- source/webapp/models.py
+++ source/webapp/models.py
@@ -6,9 +6,9 @@ STATUS_CHOICES = [('new', 'new'), ('in_progress', 'in progress'),  ('done', 'don
 class Task(models.Model):
     description = models.CharField(max_length=200, null=False, blank=False, verbose_name='description')
     status = models.CharField(max_length=50, null=False, blank=False, default='new', verbose_name='status')
-    # detailed_description = models.TextField(max_length=3000, null=False, blank=False, verbose_name='detailed_description')
-    create_until = models.DateField(auto_now_add=True, verbose_name='Create_until')
-    status = models.CharField(max_length=50, null=False, blank=False, default='new', choices=STATUS_CHOICES)
+    # detailed_description = models.TextField(max_length=3000, null=True, blank=True, verbose_name='detailed_description')
+    create_until = models.DateField(verbose_name='Create_until')
+    status = models.CharField(max_length=50, default='new', choices=STATUS_CHOICES)
 
     def __str__(self):
         return f"{self.pk}. {self.description}: {self.status}"
