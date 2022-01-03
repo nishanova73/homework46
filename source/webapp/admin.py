c7018742ade6498758b22e005971095c59713b52
Chinara <nishanova73@mail.ru>
2022-01-03 11:15:23 +0600

    Added fields of new detailed_description

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -4,10 +4,10 @@ from django.contrib import admin
 from webapp.models import Task
 
 class TaskAdmin(admin.ModelAdmin):
-    list_display = ['id', 'description', 'status', 'create_until']
+    list_display = ['id', 'description', 'detailed_description', 'status', 'create_until']
     list_filter = ['status']
     search_fields = ['description', 'status']
-    fields = ['description', 'status', 'create_until']
+    fields = ['description', 'detailed_description', 'status', 'create_until']
     readonly_fields = ['create_until']
 
 admin.site.register(Task, TaskAdmin)
