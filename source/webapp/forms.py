699a46a0bd3a06423b8621b04e6396857ec5d51a
Chinara <nishanova73@mail.ru>
2022-01-06 13:40:30 +0600

    Added form for task

---
+++ source/webapp/forms.py
@@ -0,0 +1,8 @@
+from django import forms
+
+
+class ArticleForm(forms.Form):
+    description = forms.CharField(max_length=200, required=True, label="Description")
+    detailed_description = forms.TextField(max_length=3000, required=True, label="Detailed description")
+    status = forms.CharField(max_length=15, required=True, label="Status")
+    create_until = forms.DateField(label="Create until")
