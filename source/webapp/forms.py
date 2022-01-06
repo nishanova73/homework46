ac70f11213a123f7b9921233a42e92a762345fc0
Chinara <nishanova73@mail.ru>
2022-01-06 18:48:58 +0600

    Modified select field a little bit.

--- source/webapp/forms.py
+++ source/webapp/forms.py
@@ -6,7 +6,7 @@ class TaskForm(forms.Form):
     description = forms.CharField(max_length=200, required=True, label="Description")
     detailed_description = forms.CharField(max_length=3000, required=True, label="Detailed description",
                                            widget=widgets.Textarea(attrs={"rows":5 , "cols":50}))
-    status = forms.CharField(max_length=15, required=True, label="Status",)
-                             # widget=widgets.Select)
+    status = forms.CharField(required=True, max_length=15, label="Status",
+                          widget=widgets.Select)
     create_until = forms.CharField(label="Create until",
-                                    widget=widgets.SelectDateWidget)
+                                   widget=widgets.SelectDateWidget)
